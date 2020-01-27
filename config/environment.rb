bfa14a28f185062f3641ab8b60cee914d701b32d
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-27 22:47:23 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,3 @@
  source "https://rubygems.org"
 
-gem "bundler/setup"#, git: "git@github.com:bundler-setup/bundler-setup.git"
+gem "bundler/setup", git: "git@github.com:bundler-setup/bundler-setup.git"
